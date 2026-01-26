bd4bc97117833e24379c9f725dc561f20e459518
shaitanzx <162459965+shaitanzx@users.noreply.github.com>
2026-01-26 12:48:41 +0400

    fix

--- app.py
+++ app.py
@@ -257,7 +257,7 @@ Built with [Qwen3-TTS](https://github.com/QwenLM/Qwen3-TTS) by Alibaba Qwen Team
             with gr.Tab("Voice Clone (Base)"):
                 def select_ref_audio(audio_file):
                     if audio_file:
-                        audio_path = os.path.join("reference", audio_file)
+                        audio_path = os.path.join(ROOT,"reference", audio_file)
                         base_name = os.path.splitext(audio_file)[0]
                 
                         text_content = ""
